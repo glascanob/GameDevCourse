9a68573069a5a5d4ecf8bbd38239aa717bd4d329
Gabriel <glascanob@gmail.com>
2020-08-14 16:44:25 -0500

    Add background controller
    This controller moves the background tiles from right to left to give the feeling of motion

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+
+    public int moveVel = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x != endPos.position.x)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos.position, Time.deltaTime * moveVel);
+        }
+        else
+        {
+            transform.position = startPos.position;
+        }
+    }
+}
