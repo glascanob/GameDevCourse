8480d57799d8f0ec2f6cb18f703aca42a325be46
Gabriel <glascanob@gmail.com>
2020-08-14 15:05:00 -0500

    Add player conditions for animation triggering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables
     public Animator anim;
     public Rigidbody2D rb;
 
     public int jumpForce;
+
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    //Update method used for physics in our game, because it happens every fixed amoun of time
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetKeyDown(KeyCode.Space))
+       if(grounded && Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger("Jump");
             rb.AddForce(Vector2.up * jumpForce);
         }
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
     }
 }
