b3960a72d814f26454d6bea2b20b5c5eaada6a0b
Gabriel <glascanob@gmail.com>
2020-08-18 15:51:02 -0500

    Add Logic for spawning collectables on platforms

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -10,6 +10,12 @@ public class PlatformController : MonoBehaviour
     public int moveVel = 5;
 
     Transform startPosition;
+
+    public Transform[] coinSpawnPositions;
+
+    public GameObject coinPrefab;
+
+    GameObject spawnedCoin;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,8 +31,25 @@ public class PlatformController : MonoBehaviour
         }
         else
         {
+            if(spawnedCoin != null)
+            {
+                Destroy(spawnedCoin);
+            }
             startPosition = spawnPositions[Random.Range(0, spawnPositions.Length)];
             transform.position = startPosition.position;
+
+            int coin = Random.Range(0, 3);
+            Debug.Log("The coin is" + coin);
+            switch(coin)
+            {
+                case 0:
+                case 1:
+                    spawnedCoin = Instantiate(coinPrefab, coinSpawnPositions[coin].position, Quaternion.identity,transform);
+                    break;
+                case 2:
+                default:
+                    break;
+            }
         }
     }
 }
