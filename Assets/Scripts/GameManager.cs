0639b113dac347f402e16eea14f93904db7c937e
Gabriel <glascanob@gmail.com>
2020-08-18 15:56:33 -0500

    Add Score UI to display player collectables score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     public int currentScore = 0;
+
+    public Text score;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        score.text = currentScore + "";
     }
 
     public void AddScore()
